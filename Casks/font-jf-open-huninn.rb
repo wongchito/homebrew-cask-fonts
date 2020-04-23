573d8d40ec36ffcdd3346664109c6642091b89cd
Chito Wong <9390786+wongchito@users.noreply.github.com>
2020-04-23 15:15:28 +0800

    Update jf-open-huninn

--- Casks/font-jf-open-huninn.rb
+++ Casks/font-jf-open-huninn.rb
@@ -3,10 +3,10 @@ cask 'font-jf-open-huninn' do
   sha256 '1b5a8d0204f5763408609d5e8182455d23a2ebc7450730aed19cc13c619bca37'
 
   # raw.githubusercontent.com/justfont/open-huninn-font/ was verified as official when first introduced to the cask
-  url "https://github.com/justfont/open-huninn-font/releases/download/v1.1/jf-openhuninn-#{version}.zip"
+  url "https://github.com/justfont/open-huninn-font/releases/download/v#{version}/jf-openhuninn-#{version}.zip"
   name 'jf open huninn'
   name 'jf open 粉圓'
   homepage 'https://justfont.com/huninn/'
 
-  font "jf-openhuninn-#{version}.ttf"
+  font "jf-openhuninn-#{version}/jf-openhuninn-#{version}.ttf"
 end
